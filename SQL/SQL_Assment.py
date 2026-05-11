1a922b2d06dd782149f7497d52fbdb886d365f34
sohail2025zahidi <24355@burnside.school.nz>
2026-05-12 11:05:29 +1200

    tested data base

--- SQL/SQL_Assment.py
+++ SQL/SQL_Assment.py
@@ -3,4 +3,11 @@
 #This bit of code is to connect my SQl data base to python
 import sqlite3
 db = sqlite3.connect("3DPRINTER.DB")
+cursor = db.cursor()
 
+#this is a Query test
+sql = "SELECT * FROM PRINTER_3D;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+db.close
