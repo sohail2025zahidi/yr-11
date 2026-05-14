dd64d51368ea2d21d9e63d97abe509cb1b333244
sohail2025zahidi <24355@burnside.school.nz>
2026-05-14 12:30:39 +1200

    added another option for the main menu

--- SQL/SQL_Assment.py
+++ SQL/SQL_Assment.py
@@ -16,19 +16,38 @@ def print_all_printer():
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through all the results
-    print('Printer_name                  Brand          Type           Price          Weight         Nozzle_Diaeter')
+    print('Printer_name                  Brand          Type           Price          Weight         Nozzle_Diameter')
     for printer in results:
         print(f"{printer[1]:<30}{printer[2]:<15}{printer[3]:<15}{printer[4]:<15}{printer[5]:<15}{printer[6]:<15}")
     #loop is finished here
     db.close()
 
 
+def print_printer_less_than_1000():
+    '''select all data from 3d printer costing less that $1000'''
+    db = sqlite3.connect("3DPRINTER.DB")
+    cursor = db.cursor()
+    sql = "SELECT * FROM PRINTER_3D WHERE Price < 1000;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print('Printer_name                  Brand          Type           Price          Weight         Nozzle_Diameter')
+    for printer in results:
+        print(f"{printer[1]:<30}{printer[2]:<15}{printer[3]:<15}{printer[4]:<15}{printer[5]:<15}{printer[6]:<15}")
+    #loop is finished here
+    db.close()
+
+    
+
+
 #main code
 while True:
-    user_input = input("\nWhat would you like to do.\n1. Print all 3D printers\n2.Exit\n")
+    user_input = input("\nWhat would you like to do.\n1. Print all 3D printers\n2. Select all data from 3d printer costing less that $1000\n7. Exit\n")
     if user_input == '1':
         print_all_printer()
-    if user_input == '2':
+    elif user_input == '2':
+        print_printer_less_than_1000()
+    elif user_input == '7':
         break
     else:
-        print('Not an option little bro')
+        print('Not an option\n')
